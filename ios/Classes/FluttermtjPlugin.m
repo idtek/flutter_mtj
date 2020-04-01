4a18690af3b8208920a5b5e8ef9d0df22e20d1dd
jybridge <996030870@qq.com>
2020-04-01 14:08:08 +0800

    java and oc

--- ios/Classes/FluttermtjPlugin.m
+++ ios/Classes/FluttermtjPlugin.m
@@ -1,15 +1,20 @@
 #import "FluttermtjPlugin.h"
-#if __has_include(<fluttermtj/fluttermtj-Swift.h>)
-#import <fluttermtj/fluttermtj-Swift.h>
-#else
-// Support project import fallback if the generated compatibility header
-// is not copied when this plugin is created as a library.
-// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "fluttermtj-Swift.h"
-#endif
 
 @implementation FluttermtjPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFluttermtjPlugin registerWithRegistrar:registrar];
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"fluttermtj"
+            binaryMessenger:[registrar messenger]];
+  FluttermtjPlugin* instance = [[FluttermtjPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
 @end
