528324a47a983e58424d593db16326909e997dbd
jybridge <996030870@qq.com>
2020-07-27 14:50:36 +0800

    埋点只针对正式环境

--- lib/fluttermtj.dart
+++ lib/fluttermtj.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class Fluttermtj {
+  static bool isRelease = false;
   static const MethodChannel _channel =
       const MethodChannel('fluttermtj');
 
@@ -12,8 +13,10 @@ class Fluttermtj {
   }
 
   // 初始化百度移动统计平台 
-  static void StartBaiduMobileStat(String appId) {
+  static void StartBaiduMobileStat(String appId, bool currentEnv) {
     assert(appId != null);
+    isRelease = currentEnv;
+    if (!currentEnv) return; 
     List<dynamic> params = [appId];
     _channel.invokeMethod('StartBaiduMobileStat', params);
   }
@@ -26,6 +29,7 @@ class Fluttermtj {
   }
 
   static void LogEvent(String eventId) {
+    if (!isRelease) return;
     assert(eventId != null);
     List<dynamic> params = [eventId];
     _channel.invokeMethod('LogEvent', params);
@@ -33,6 +37,7 @@ class Fluttermtj {
 
   //需要在开始和结束时都调用
   static void LogEventWithDurationTime(String eventId) {
+    if (!isRelease) return;
     assert(eventId != null);
     List<dynamic> params = [eventId];
     _channel.invokeMethod('LogEventWithDurationTime', params);
@@ -40,24 +45,28 @@ class Fluttermtj {
 
   // 指定事件触发次数
   static void LogEventWithNumberOfTime(String eventId, int time) {
+    if (!isRelease) return;
     assert(eventId != null);
     List<dynamic> params = [eventId, time];
     _channel.invokeMethod('LogEventWithNumberOfTime', params);
   }
   
   static void LogEventWithOneSecond(String eventId) {
+    if (!isRelease) return;
     assert(eventId != null);
     List<dynamic> params = [eventId];
     _channel.invokeMethod('LogEventWithOneSecond', params);
   }
 
   static void LogEventWithAttribute(String eventId, Map<String, String> map) {
+    if (!isRelease) return;
     assert(eventId != null);
     List<dynamic> params = [eventId, map];
     _channel.invokeMethod('LogEventWithAttribute', params);
   }
 
   static void LogEventWithOneSecondAndAttributes(String eventId, Map<String, String> map) {
+    if (!isRelease) return;
     assert(eventId != null);
     List<dynamic> params = [eventId, map];
     _channel.invokeMethod('LogEventWithOneSecondAndAttributes', params);
@@ -65,18 +74,21 @@ class Fluttermtj {
 
   //需要在开始和结束时都调用
   static void LogEventWithDurationTimeAndAttributes(String eventId, {Map<String, String> map}) {
+    if (!isRelease) return;
     assert(eventId != null);
     List<dynamic> params = [eventId, map];
     _channel.invokeMethod('LogEventWithDurationTimeAndAttributes', params);
   }
 
   static void PageviewStartWithName(String pageName) {
+    if (!isRelease) return;
     assert(pageName != null);
     List<dynamic> params = [pageName];
     _channel.invokeMethod('PageviewStartWithName', params);
   }
 
   static void PageviewEndWithName(String pageName) {
+    if (!isRelease) return;
     assert(pageName != null);
     List<dynamic> params = [pageName];
     _channel.invokeMethod('PageviewEndWithName', params);
